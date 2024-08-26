43c1493f3566bd9c1a6589e063fa4d78ce06a4ca
Keygenw <gabriel.henri.sil@gmail.com>
2024-08-25 22:25:21 -0300

    feat: settings and small changes

---
+++ frontend/src/app/machines/page.tsx
@@ -0,0 +1,52 @@
+// src/app/machines/page.tsx
+"use client";
+
+import React, { useState } from 'react';
+import Aside from '../components/aside';
+import Footer from '../components/footer';
+import Table from '../components/Table';
+import MachineForm from '../components/machineform';
+
+interface Machine {
+  name: string;
+  type: string;
+  model: string;
+  manufactureDate: string;
+  serialNumber: string;
+  location: string;
+  maintenanceHistory: string;
+}
+
+const initialMachines: Machine[] = [
+  { name: 'Lathe', type: 'CNC', model: 'X1', manufactureDate: '2020-05-10', serialNumber: 'L123', location: 'Shop Floor', maintenanceHistory: 'Regular' },
+  // Adicione mais máquinas conforme necessário
+];
+
+const MachinePage: React.FC = () => {
+  const [machines, setMachines] = useState<Machine[]>(initialMachines);
+
+  const handleAddMachine = (machine: Machine) => {
+    setMachines([...machines, machine]);
+  };
+
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <Aside />
+
+        <main className="flex-1 flex flex-col p-6 ml-64"> {/* ml-64 para dar espaço ao Aside */}
+          <h1 className="text-4xl font-bold mb-4">Machines</h1>
+          <MachineForm onAddMachine={handleAddMachine} />
+          <Table
+            headers={['Name', 'Type', 'Model', 'Manufacture Date', 'Serial Number', 'Location', 'Maintenance History']}
+            data={machines}
+          />
+        </main>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default MachinePage;
