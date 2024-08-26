f94135a956ebaf1c07c41c0c266b2c7f21540f69
Keygenw <gabriel.henri.sil@gmail.com>
2024-08-25 22:26:29 -0300

    feat: implementation of dashboards home

---
+++ frontend/src/app/components/Charts.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement
+} from 'chart.js';
+
+// Registro dos elementos necessários para o gráfico
+ChartJS.register(LineElement, CategoryScale, LinearScale, Title, Tooltip, Legend, PointElement);
+
+const Chart: React.FC = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Monthly Sales',
+        data: [30, 20, 50, 40, 60, 70, 90],
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        fill: true,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem: any) {
+            return `Sales: $${tooltipItem.raw}`;
+          },
+        },
+      },
+    },
+    layout: {
+      padding: 10, 
+    },
+  };
+
+  return (
+    <div style={{ width: '50%', maxWidth: '500px', margin: 'auto' }}>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default Chart;
+
